5983ffda9413fde21169e16f42f27290134def16
MoonStdio <juxiupan@163.com>
2025-03-18 08:23:53 +0800

    feat:项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: MoonStdio juxiupan@163.com
+ * @Date: 2025-03-18 08:18:18
+ * @LastEditors: MoonStdio juxiupan@163.com
+ * @LastEditTime: 2025-03-18 08:21:13
+ * @FilePath: \vite-project\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>app</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: MoonStdio juxiupan@163.com
+ * @Date: 2025-03-18 08:18:18
+ * @LastEditors: MoonStdio juxiupan@163.com
+ * @LastEditTime: 2025-03-18 08:22:30
+ * @FilePath: \vite-project\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
